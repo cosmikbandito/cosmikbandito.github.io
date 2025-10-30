5dd48f6599656122e86f9a61430e8aa43abf0994
cosmikbandito <cosmikbandito@gmail.com>
2025-10-30 09:51:43 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const monImage = document.querySelector("img");
 
 monImage.addEventListener("click", () => {
   const maSrc = monImage.getAttribute("src");
-  if (maSrc === "images/YouAndMe.png") {
-    monImage.setAttribute("src", "images/ThisIsUs.png");
-  } else {
-    monImage.setAttribute("src", "images/YouAndMe.png");
+  if (maSrc === "images/YouAndMe.png") 
+  {monImage.setAttribute("src", "images/ThisIsUs.png");} 
+  else 
+  {monImage.setAttribute("src", "images/YouAndMe.png");
   }
 });
